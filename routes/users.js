64f017a8161558929a71177447fb6d9e6b440328
Christian Cozma <christiancozma.work@gmail.com>
2021-04-05 17:52:29 -0500

    modify: index.js, user.js, home-navbar.pug, add: login.pug

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//TODO: Set up the GET routes:
+router.get("/login", (req, res) => {
+
+    res.render('login', {title: "Login"})
+})
+
+// router.get('')
 module.exports = router;
