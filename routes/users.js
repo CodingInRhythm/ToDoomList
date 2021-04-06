e4c55b5199ab4da915ba05f65d99549cbd45321d
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-06 10:04:56 -0700

    Added form destructuring + bug fixes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('./db/models')
+const db = require('../db/models')
 const csrf = require('csurf')
 // const { Villains } = db;
 
@@ -10,18 +10,20 @@ const csrfProtection = csrf({ cookie: true });
 //TODO: Set up the GET routes:
 router.get("/login", csrfProtection, (req, res) => {
     //let user = Villains.create();
-    res.render('login', {title: "Login", csrfToken = req.csrfToken()})
+    res.render('login', {pageTitle: "Login", csrfToken: req.csrfToken()})
 })
 
 router.post("/login", csrfProtection, (req, res) => {
+  const { username, password } = req.body();
   res.redirect("/")
 })
 
 router.get("/sign-up", csrfProtection, (req, res) => {
-  res.render('sign-up', {title: "Sign Up", csrfToken = req.csrfToken()});
+  res.render('sign-up', {pageTitle: "Sign Up", csrfToken: req.csrfToken()});
 })
 
 router.post("/sign-up", csrfProtection, (req, res) => {
+  const { title, firstName, lastName, username, email, password, confirmPassword } = req.body();
   res.redirect("/");
 })
 
