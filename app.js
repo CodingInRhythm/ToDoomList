c7827c5fca4ee457d3db83e89ab6b489b3dd588d
Alex Clough <alexcl123@gmail.com>
2021-04-06 19:29:20 -0400

    working on app page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const appRouter = require('./routes/app')
 
 const app = express();
 
@@ -37,6 +38,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/app', appRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
