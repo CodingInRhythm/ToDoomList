f8d75f97c1588833c3d9c2c7046ae67037c2eb6b
Alex Clough <alexcl123@gmail.com>
2021-04-07 12:07:50 -0400

    trying out connection

--- config/database.js
+++ config/database.js
@@ -8,7 +8,12 @@ module.exports = {
     password,
     database,
     host,
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+    dialect: "postgres",
+    seederStorage: "sequelize",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
   },
 };
