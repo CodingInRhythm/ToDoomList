5fbfce3127164ada490b7901a6780586145f3a64
nathanieldcooke <nathanield.cooke@gmail.com>
2021-04-07 13:21:39 -0700

    requires and orders,  middlesware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const { sequelize } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sessionSecret } = require('./config')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -16,21 +17,21 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// set up session middleware
-const store = new SequelizeStore({ db: sequelize });
-
-app.use(
-  session({
-    secret: 'superSecret',
+app.use(cookieParser(sessionSecret))
+app.use(session({
+    secret: sessionSecret,
     store,
     saveUninitialized: false,
     resave: false,
   })
 );
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up session middleware
+const store = new SequelizeStore({ db: sequelize });
+
 
 // create Session table if it doesn't already exist
 store.sync();
