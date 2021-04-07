34f60aa99884d65ef50db78d7931b77e830f7f76
Alex Clough <alexcl123@gmail.com>
2021-04-07 14:17:47 -0400

    setting up CRUD routes

--- routes/app.js
+++ routes/app.js
@@ -1,9 +1,47 @@
 const express = require("express");
+const { db } = require("../config");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("app", { title: "To Doom List" });
-});
+// router.get("/", function (req, res, next) {
+//   const schemes = fetch("/schemes")
+//   //todo: add tasks
+//   res.render("app", { tasks, schemes });
+// });
+router.post("/schemes", (req, res) => {
+  //destructure req to get Scheme name and Villain id
+  const scheme = await db.Scheme.create()
+  res.json(scheme)
+})
 
+
+router.get("/schemes/:schemeid", async (req, res) => {
+  const id = req.params.id
+  const scheme = await db.Scheme.findByPk(id)
+  res.json()
+})
+
+router.put("/schemes/:schemeid", (req, res) => {
+  
+})
+
+router.delete("/schemes/:schemeid", (req, res) => {
+
+})
+
+router.post("/ploys", (req, res) => {
+  res.json("/", {})
+})
+
+router.get("/ploys/:ployid", (req, res) => {
+  await 
+  res.json()
+})
+
+router.put("/ploys/:ployid", (req, res) => {
+  res.render("app", {})
+})
+router.delete("/ploys/:ployid", (req, res) => {
+  res.json()
+})
 module.exports = router;
