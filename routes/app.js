dfffc19f56f7bd8851de82cdb7d8e7bd2a3e13e0
Alex Clough <alexcl123@gmail.com>
2021-04-07 17:14:51 -0400

    finishing up routes

--- routes/app.js
+++ routes/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require('../db/models')
 const router = express.Router();
 
-const db = require('../db/models')
+
 
 /* GET home page. */
 // router.get("/", function (req, res, next) {
@@ -10,26 +10,37 @@ const db = require('../db/models')
 //   //todo: add tasks
 //   res.render("app", { tasks, schemes });
 // });
+
+//works
 router.post("/schemes", async (req, res) => {
-  //destructure req to get Scheme name and Villain id
-  const scheme = await db.Scheme.create()
+   const { name, villainId } = req.body;
+  const scheme = await db.Scheme.create({
+    name,
+    villainId
+  })
   res.json({scheme})
 })
 
-
+//works
 router.get("/schemes/:schemeid", async (req, res) => {
   const id = parseInt(req.params.schemeid, 10)
   const scheme = await db.Scheme.findByPk(id)
   res.json({scheme})
 })
 
+//works
 router.put("/schemes/:schemeid", async (req, res) => {
+  const { name, villainId } = req.body;
   const id = parseInt(req.params.schemeid, 10)
   const scheme = await db.Scheme.findByPk(id) 
-  await scheme.update()
+  await scheme.update({
+    name,
+    villainId
+  })
   res.json({scheme})
 })
 
+//cannot delete a scheme without deleting its ploys.  How to include ploys?
 router.delete("/schemes/:schemeid", async (req, res) => {
   const id = parseInt(req.params.schemeid, 10)
   const scheme = await db.Scheme.findByPk(id)
@@ -38,25 +49,37 @@ router.delete("/schemes/:schemeid", async (req, res) => {
   res.status(204).end()
 })
 
-router.post("/ploys", (req, res) => {
-  //destructure req to get Scheme name and Villain id
-  const ploy = await db.Ploy.create()
+//works
+router.post("/ploys", async (req, res) => {
+  const {name, schemeId} = req.body
+  const ploy = await db.Ploy.create({
+    name,
+    schemeId
+  })
   res.json({ploy})
 })
 
-router.get("/ploys/:ployid", (req, res) => {
+//works
+router.get("/ploys/:ployid", async (req, res) => {
   const id = parseInt(req.params.ployid, 10)
   const ploy = await db.Ploy.findByPk(id) 
   res.json({ploy})
 })
 
-router.put("/ploys/:ployid", (req, res) => {
+//works
+router.put("/ploys/:ployid", async (req, res) => {
+  const {name, schemeId} = req.body
   const id = parseInt(req.params.ployid, 10)
   const ploy = await db.Ploy.findByPk(id)
-  await ploy.update()
+  await ploy.update({
+    name,
+    schemeId
+  })
   res.json({ploy})
 })
-router.delete("/ploys/:ployid", (req, res) => {
+
+//works
+router.delete("/ploys/:ployid", async (req, res) => {
   const id = parseInt(req.params.ployid, 10)
   const ploy = await db.Ploy.findByPk(id)
   await ploy.destroy()
