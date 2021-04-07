eb9fc83784386889ce0e0f5082856108835c25af
Alex Clough <alexcl123@gmail.com>
2021-04-07 12:41:13 -0400

    heroku issue

--- config/database.js
+++ config/database.js
@@ -14,6 +14,13 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    protocol: "postgres",
     seederStorage: "sequelize",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    }
   },
 };
