cbef893a3b166a9f73d89b4343c0693711c03ae9
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-04-10 21:27:50 -0700

    Added some comments

--- public/javascripts/ploy-query.js
+++ public/javascripts/ploy-query.js
@@ -1,5 +1,6 @@
 import Ploys from "./ploys.js";
 
+//This class tracks what queries for Ploys have been called, can replay the last query used
 class PloyQuery{
     constructor(){
         let queryType = null;
@@ -8,6 +9,7 @@ class PloyQuery{
 
     //queryType should be either "schemeId", "all" (if we want to implement?), "search"
     //queryData will be id, null, search string respectively
+    //Use this instead of calling Ploys queries directly
     async makeNewQuery(type, data){
         this.queryType = type;
         this.queryData = data;
@@ -21,10 +23,12 @@ class PloyQuery{
         }
     }
 
+    //Returns data from last call of makeNewQuery();
     getLastQuery(){
         return {queryType: this.queryType, queryData: this.queryData};
     }
 
+    //Replays the last call of makeNewQuery
     async callLastQuery(){
         let prev = this.getLastQuery();
         return await this.makeNewQuery(prev.queryType, prev.queryData);
