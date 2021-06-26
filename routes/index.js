2be4b80821c7b5ea7770773a1805f5eb030c673e
nathanieldcooke <nathanield.cooke@gmail.com>
2021-06-26 08:40:23 -0700

    route user straight to application, when demo button it clicked

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { csrfProtection, asyncHandler } = require('../utils/utils');
+const { requireAuth } = require("../auth/auth.js")
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'To Doom List' });
+router.get('/', csrfProtection, function(req, res, next) {
+  res.render('index', { title: 'To Doom List', csrfToken: req.csrfToken() });
 });
 
 module.exports = router;
