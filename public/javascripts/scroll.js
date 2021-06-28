b0eddd98712e82be8f74635f0fecb3acf65311c7
Alex Clough <alexcl123@gmail.com>
2021-06-28 14:20:27 -0400

    working on scroll media queries

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -1,75 +1,79 @@
-export default class Scroll {
-    constructor() {
+// export default class Scroll {
+//     constructor() {
 
-    this.unScrolled = document.querySelector('.un-scrolled')
-    this.header = document.querySelector('header')
+//     this.unScrolled = document.querySelector('.un-scrolled')
+//     this.header = document.querySelector('header')
 
-    this.frame1 = document.querySelector('.frame-1');
-    this.frame2 = document.querySelector('.frame-2');
-    this.frame3 = document.querySelector('.frame-3');
-    this.frame4 = document.querySelector('.frame-4');
+//     this.frame1 = document.querySelector('.frame-1');
+//     this.frame2 = document.querySelector('.frame-2');
+//     this.frame3 = document.querySelector('.frame-3');
+//     this.frame4 = document.querySelector('.frame-4');
 
-    this.playNext = true;
+//     this.playNext = true;
 
-    this.animationNames = [
-        {
-            animationName: 'blue-lightblue',
-            frameOut: this.frame1,
-            frameIn: this.frame2,
-            color: '#3292f2'
-        },
-        {
-            animationName: 'lightblue-violet',
-            frameOut: this.frame2,
-            frameIn: this.frame3,
-            color: '#6a4ca6'
-        },
-        {
-            animationName: 'violet-green',
-            frameOut: this.frame3,
-            frameIn: this.frame4,
-            color: '#5bb84d'
+//     this.animationNames = [
+//         {
+//             animationName: 'blue-lightblue',
+//             frameOut: this.frame1,
+//             frameIn: this.frame2,
+//             color: '#3292f2'
+//         },
+//         {
+//             animationName: 'lightblue-violet',
+//             frameOut: this.frame2,
+//             frameIn: this.frame3,
+//             color: '#6a4ca6'
+//         },
+//         {
+//             animationName: 'violet-green',
+//             frameOut: this.frame3,
+//             frameIn: this.frame4,
+//             color: '#5bb84d'
 
-        },
-        {
-            animationName: 'green-blue',
-            frameOut: this.frame4,
-            frameIn: this.frame1,
-            color: '#0060bf'
-        }
-    ]
-}
+//         },
+//         {
+//             animationName: 'green-blue',
+//             frameOut: this.frame4,
+//             frameIn: this.frame1,
+//             color: '#0060bf'
+//         }
+//     ]
+// }
 
-    play = async () => {
+//     play = async () => {
         
-        while(this.playNext) {
-            await this.timer(5000)// waits with new state for 5s, then start over agiain.
+//         while(this.playNext) {
+//             await this.timer(5000)// waits with new state for 5s, then start over agiain.
 
-            let currTransition = this.animationNames.shift()
-            this.unScrolled.style.animationName = currTransition.animationName
-            this.header.style.animationName = currTransition.animationName
+//                             //MUST UNCOMMENT AFTER DEBUGGING
+//             // let currTransition = this.animationNames.shift()
+            
+                    
 
-            currTransition.frameOut.style.animationName = 'out-of-frame';
-            currTransition.frameIn.style.animationName = 'in-to-frame';
+//             this.unScrolled.style.animationName = currTransition.animationName
+//             this.header.style.animationName = currTransition.animationName
 
-            this.unScrolled.style.backgroundColor = currTransition.color
-            this.header.style.backgroundColor = currTransition.color
+//             currTransition.frameOut.style.animationName = 'out-of-frame';
+//             currTransition.frameIn.style.animationName = 'in-to-frame';
+
+//             this.unScrolled.style.backgroundColor = currTransition.color
+//             this.header.style.backgroundColor = currTransition.color
 
 
-            currTransition.frameOut.style.left = '-250%'
-            currTransition.frameIn.style.left = '0%'
-            this.animationNames.push(currTransition)
+//             currTransition.frameOut.style.left = '-250%'
+//             currTransition.frameIn.style.left = '0%'
+//             this.animationNames.push(currTransition)
 
-        }
+//         }
         
-    }
+//     }
 
-    timer = async (time) => {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                resolve('resolved');
-            }, time);
-        });
-    }
+//     timer = async (time) => {
+//         return new Promise(resolve => {
+//             setTimeout(() => {
+//                 resolve('resolved');
+//             }, time);
+//         });
+//     }
 
-}
+// }
